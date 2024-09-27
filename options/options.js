a2499c6a952da1e9541faccd744874912019f7dd
Crowdbotics <engineering@crowdbotics.com>
2024-09-27 11:11:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sumanregression6-841",
+  url: "https://sumanregression6-841.botics.co",
+  api: "https://sumanregression6-841.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
